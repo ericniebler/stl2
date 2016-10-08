68e5139789b1da24de89048d31f6416e5877a866
Eric Niebler <eniebler@boost.org>
2016-10-08 12:29:35 -0700

    proxy iterators review feedback, customization point objects, update for N4569

---
+++ ext/proxy/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+pandoc -f markdown_github -t html -o appendices.html --number-sections -s -S appendices.md
+pandoc -f markdown_github+yaml_metadata_block+citations -t html -o P0022R2.html --filter pandoc-citeproc --csl=acm-sig-proceedings.csl --number-sections --toc -s -S --template=pandoc-template --include-before-body=header.html --include-after-body=appendices.html --include-in-header=pandoc.css Dxxxx.md
